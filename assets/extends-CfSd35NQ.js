df3e8bfb2ab098f40767832152193a19421f0ea0
vebgenix08 <vebgenix08@users.noreply.github.com>
2026-02-19 04:24:58 +0000

    deploy: bddf2075959c129633f28f99e73934cd2abb0075

--- assets/extends-CfSd35NQ.js
+++ assets/extends-CfSd35NQ.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as av,j as Dn}from"./index-hzCjIRQX.js";/**
+import{r as Ye,g as av,j as Dn}from"./index-CFpyYBrN.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
