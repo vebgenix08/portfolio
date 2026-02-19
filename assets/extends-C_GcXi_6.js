bff3acfd8d92291c667938815af427e60b40ba19
vebgenix08 <vebgenix08@users.noreply.github.com>
2026-02-19 04:21:57 +0000

    deploy: d6743a3a16b2d79c816e0eba197a796c84e6e2f7

--- assets/extends-C_GcXi_6.js
+++ assets/extends-C_GcXi_6.js
@@ -1,4 +1,4 @@
-import{r as Ye,g as av,j as Dn}from"./index-BWuAJrVO.js";/**
+import{r as Ye,g as av,j as Dn}from"./index-hzCjIRQX.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
